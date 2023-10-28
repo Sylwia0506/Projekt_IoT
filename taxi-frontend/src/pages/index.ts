720f17b305fe28dc217f69423f75ff175467f61f
Paweł Awramiuk <awramiukofficial@gmail.com>
2023-10-28 10:19:32 +0200

    Added taxi view

--- taxi-frontend/src/pages/index.ts
+++ taxi-frontend/src/pages/index.ts
@@ -1,4 +1,4 @@
 export { default as Home } from "./home/Home.tsx"
 export { default as Map } from "./map/Map.tsx"
-export { default as Taxi } from "./taxi/Taxi.tsx"
+export { default as Taxi } from "./taxi/Taxis.tsx"
 export { default as Drivers } from "./drivers/Drivers.tsx"
