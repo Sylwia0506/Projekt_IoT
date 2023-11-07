7e92d9a8a470241ce3114193b9e4b8bc9f0fbf66
Szymon Laskowski <75802336+Lask0v@users.noreply.github.com>
2023-11-07 22:20:14 +0100

    Simulator - fix schedulers #20 (#22)
    * Simulator - fix schedulers #20

---
+++ simulator/logger.py
@@ -0,0 +1,41 @@
+import logging
+import sys
+
+
+class Logger:
+    def __init__(self, log_file_name, log_level=logging.DEBUG):
+        self.log_file_name = log_file_name
+        self.log_level = log_level
+        self.logger = self.setup_logger()
+
+    def setup_logger(self):
+        logger = logging.getLogger(__name__)
+        logger.setLevel(self.log_level)
+        ##TODO moze jakis informator jaki to symulator?
+        formatter = logging.Formatter('%(levelname)s - %(asctime)s: -  %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
+        file_handler = logging.FileHandler(self.log_file_name)
+        file_handler.setFormatter(formatter)
+
+        console_handler = logging.StreamHandler(sys.stdout)
+        console_handler.setFormatter(formatter)
+
+        logger.addHandler(file_handler)
+        logger.addHandler(console_handler)
+
+        return logger
+
+    def debug(self, message):
+        self.logger.debug(message)
+
+    def info(self, message):
+        self.logger.info(message)
+
+    def warning(self, message):
+        self.logger.warning(message)
+
+    def error(self, message):
+        self.logger.error(message)
+
+    def critical(self, message):
+        self.logger.critical(message)
