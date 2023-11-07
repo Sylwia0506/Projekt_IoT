5c32203facc45da80b6e405646bd884fff11137c
Roger1121 <leszczynski_szymon@wp.pl>
2023-11-07 19:49:00 +0100

    fix

--- taxi-backend/taxi_backend/settings.py
+++ taxi-backend/taxi_backend/settings.py
@@ -137,7 +137,7 @@ from taxi_backend.views import on_message
 
 mqtt_client = mqtt.Client()
 mqtt_client.on_message = on_message
-# mqtt_client.connect("mqtt", 1883)
+mqtt_client.connect("mqtt", 1883)
 topic = "uber/coords"
-# mqtt_client.subscribe(topic)
-# mqtt_client.loop_start()
+mqtt_client.subscribe(topic)
+mqtt_client.loop_start()
