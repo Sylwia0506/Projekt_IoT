0f835e0b3b20c8f0383e6adeaa59b32434572a09
Piotr G <81086063+piogru@users.noreply.github.com>
2023-11-14 23:33:15 +0100

    Move OverlayDrawer away from screen edge

--- taxi-frontend/src/components/overlay-drawer/OverlayDrawer.tsx
+++ taxi-frontend/src/components/overlay-drawer/OverlayDrawer.tsx
@@ -1,42 +1,43 @@
-import { ReactElement, ReactNode } from "react"
-import { IconButton, Drawer, Box, SwipeableDrawer } from "@mui/material"
+import { ReactNode } from "react"
+import { IconButton, Drawer, Box } from "@mui/material"
 import StartIcon from "@mui/icons-material/Start"
 
 type OverlayDrawerProps = {
   minWidth: number | string
   open: boolean
-  onOpen: () => void
   onClose: () => void
-  // children: ReactElement
   children: ReactNode
 }
 
 const OverlayDrawer = ({
   minWidth = 200,
   open,
-  onOpen,
   onClose,
   children,
 }: OverlayDrawerProps) => {
   return (
-    // <SwipeableDrawer
     <Drawer
       anchor={"right"}
       open={open}
-      // onOpen={onOpen}
       onClose={onClose}
       hideBackdrop
       variant="persistent"
       ModalProps={{
         keepMounted: false,
       }}
-      PaperProps={{ sx: { top: "auto" } }}
+      PaperProps={{
+        sx: {
+          height: "calc(100% - 64px - 32px)",
+          top: "auto",
+          margin: 2,
+          borderRadius: 2,
+        },
+      }}
     >
       <Box sx={{ minWidth: minWidth }}>
         <IconButton onClick={onClose} sx={{ margin: 1 }}>
           <StartIcon />
         </IconButton>
-
         {children}
       </Box>
     </Drawer>
