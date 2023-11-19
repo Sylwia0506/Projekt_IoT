f9eb5d52b8f2dc3abb84f485272a307d274ec9ff
Roger1121 <leszczynski_szymon@wp.pl>
2023-11-19 11:23:01 +0100

    seeded db and added course endpoints

--- taxi-backend/taxi_backend/serializers.py
+++ taxi-backend/taxi_backend/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Taxi
+from .models import Taxi, Course
 
 class TaxiSerializer(serializers.ModelSerializer):
     class Meta:
         model = Taxi
         fields = ["id", "brand", "model", "registration", "vinNumber", "seatCount", "isAvailable"]
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = ["id", "startLatitude", "startLongitude", "endLatitude", "endLongitude", "passengerCount", "taxi", "fare"]
