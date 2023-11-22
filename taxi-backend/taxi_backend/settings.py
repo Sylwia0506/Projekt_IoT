be97d1ab1063a52f55946382b4e7795ced4a120c
sborys00 <16275683+sborys00@users.noreply.github.com>
2023-11-22 08:19:18 +0100

    Update postgres healthcheck

--- taxi-backend/taxi_backend/settings.py
+++ taxi-backend/taxi_backend/settings.py
@@ -135,9 +135,11 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 import paho.mqtt.client as mqtt
 from taxi_backend.views import on_message
 
+print("Started")
+
 mqtt_client = mqtt.Client()
-#mqtt_client.on_message = on_message
-#mqtt_client.connect("mqtt", 1883)
+mqtt_client.on_message = on_message
+mqtt_client.connect("mqtt", 1883)
 topic = "uber/coords"
-#mqtt_client.subscribe(topic)
-#mqtt_client.loop_start()
+mqtt_client.subscribe(topic)
+mqtt_client.loop_start()
