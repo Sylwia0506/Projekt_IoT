505c57162cff7fc892f3163a34d1d15f7caa874e
akalinowski <a.kalinowski000@gmail.com>
2023-11-25 17:05:02 +0100

    #71 redux store configuration

--- taxi-frontend/src/main.tsx
+++ taxi-frontend/src/main.tsx
@@ -4,12 +4,16 @@ import App from "./App.tsx"
 import "./index.css"
 import { CssBaseline, ThemeProvider } from "@mui/material"
 import theme from "./assets/theme.ts"
+import { Provider } from "react-redux"
+import { store } from "./store/store.ts"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 )
