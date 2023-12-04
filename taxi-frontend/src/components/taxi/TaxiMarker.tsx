e2b86a9458df8dc29c10097b50e0433c3a84e098
Piotr G <81086063+piogru@users.noreply.github.com>
2023-12-04 22:09:27 +0100

    Replace placeholder car svg

--- taxi-frontend/src/components/taxi/TaxiMarker.tsx
+++ taxi-frontend/src/components/taxi/TaxiMarker.tsx
@@ -10,26 +10,58 @@ type TaxiMarkerProps = {
 }
 
 export default function TaxiMarker({ taxi, selected }: TaxiMarkerProps) {
-  const color = selected ? "yellow" : "red";
   const svgIcon = L.divIcon({
     html: `
-    <svg
-          width="20"
-          height="20"
-          viewBox="0 0 20 20"
-          // fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          transform="rotate(${taxi.rotation})"
-        >
-          <circle cx="10" cy="10" r="10" fill=${color} />
-          <circle cx="10" cy="3" r="2" fill="aquamarine" />
-        </svg>`,
+      <svg
+        width="32"
+        height="38"
+        xmlns="http://www.w3.org/2000/svg"
+        transform="rotate(${taxi.rotation})"
+      >
+        <g>
+          <rect
+            rx="5"
+            height="38"
+            width="20"
+            y="0"
+            x="6"
+            fill="${selected ? "blue" : "black"}"
+          />
+          <path d="
+              M 8 13
+              A 12 5 0 0 1 24 13
+              L 23 20
+              L 9 20
+              Z
+            "
+            fill="white"
+          />
+          <path d="
+              M 11.5 0.5
+              a 5 4 0 0 0 -5 4
+              l 5 0
+              l 1 0
+              l 0 -4
+              Z
+            "
+            fill="${selected ? "yellow" : "white"}"
+          />
+          <path d="
+              M 25.5 4.5
+              a 5 4 0 0 0 -5 -4
+              l -1 0
+              l 0 4
+              Z
+            "
+            fill="${selected ? "yellow" : "white"}"
+          />
+        </g>
+      </svg>`,
     className: "svg-icon",
     iconSize: [20, 20],
     iconAnchor: [20, 20],
   })
 
-  // TODO: custom icon
   return (
     <Marker key={taxi.VIN} position={taxi.center} icon={svgIcon}>
       <Popup>{`${taxi.licensePlate} - ${taxi.producent} ${taxi.model}`}</Popup>
