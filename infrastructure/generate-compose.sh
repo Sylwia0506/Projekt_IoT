40b00f8433b7ee598a88b798089c2b81329aa081
sborys00 <16275683+sborys00@users.noreply.github.com>
2023-12-05 15:55:43 +0100

    Update infra

--- infrastructure/generate-compose.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo docker compose \
--f ../taxi-backend/docker-compose.yaml \
--f ../simulator/docker-compose.yaml \
--f ../taxi-frontend/docker-compose.yaml \
-config --no-path-resolution --no-interpolate \
-> docker-compose.yaml
