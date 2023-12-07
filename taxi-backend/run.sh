75d4335e46a9e43d8eaba2821fedd0ce9edf7d48
sborys00 <16275683+sborys00@users.noreply.github.com>
2023-12-07 13:08:14 +0100

    Update django in compose

--- taxi-backend/run.sh
+++ taxi-backend/run.sh
@@ -4,4 +4,4 @@ python3 manage.py makemigrations taxi_backend
 python3 manage.py makemigrations
 python3 /app/manage.py migrate
 python3 /app/manage.py loaddata /app/taxi_backend/seed/*.json
-python3 /app/manage.py runserver 0.0.0.0:80
+python3 /app/manage.py runserver 0.0.0.0:8000
