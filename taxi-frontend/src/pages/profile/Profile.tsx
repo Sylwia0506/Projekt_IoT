b8762c42d9389a05c6cadac2b34c3718cfd67231
akalinowski <a.kalinowski000@gmail.com>
2023-12-12 20:52:41 +0100

    #64 - profile view

--- taxi-frontend/src/pages/profile/Profile.tsx
+++ taxi-frontend/src/pages/profile/Profile.tsx
@@ -1,14 +1,17 @@
 import { FC } from "react"
 import { Avatar, Box, Paper, Stack, Typography } from "@mui/material"
+import { useAppSelector } from "../../store/hooks.ts"
 
 const Profile: FC = () => {
+  const { currentUser } = useAppSelector((state) => state.authReducer)
+
   return (
     <Box
       sx={{
         height: "100%",
       }}
     >
-      <Paper sx={{ p: 2, m: 2 }}>
+      <Paper elevation={0} sx={{ p: 2, m: 2 }}>
         <Stack>
           <Stack direction="row">
             <Avatar variant="rounded" />
@@ -18,6 +21,18 @@ const Profile: FC = () => {
               </Typography>
             </Stack>
           </Stack>
+          <Paper sx={{ m: 2, p: 2 }}>
+            <Stack gap={2}>
+              <Typography component="h2" variant="h4">
+                Informacje
+              </Typography>
+              <Typography>
+                <>
+                  <b>Nazwa</b>: {currentUser.name}
+                </>
+              </Typography>
+            </Stack>
+          </Paper>
         </Stack>
       </Paper>
     </Box>
