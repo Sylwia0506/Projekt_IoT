d56b3ebbb3d6186c92ba7125d78b6be4555c8201
UsernameAnia <ania.ania1010@wp.pl>
2024-01-18 12:37:24 +0100

    Api courses part 1

--- taxi-frontend/src/pages/courses/Courses.tsx
+++ taxi-frontend/src/pages/courses/Courses.tsx
@@ -1,12 +1,36 @@
 import { DataGrid } from '@mui/x-data-grid';
-import { columns, rows } from './mockCourses';
+import { columns } from './mockCourses';
+import { useAppDispatch, useAppSelector } from '../../store/hooks';
+import { courseLoading, courseSelector, getCourses } from '../../store/course/courseSlice';
+import { useEffect, useState } from 'react';
+import { CourseBody } from '../../store/course/types/courseTypes';
 
 
 function Courses() {
+  const dispatch = useAppDispatch()
+  const courses = useAppSelector(courseSelector)
+  const loading = useAppSelector(courseLoading)
+  const [shownCourses, setShownCourses] = useState<CourseBody[]>([])
+
+  
+  useEffect(() => {
+    void dispatch(getCourses())
+  }, [dispatch])
+
+  useEffect(() => {
+    if (courses) {
+      setShownCourses(courses.slice())
+    }
+  }, [courses])
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
+  
   return (
     <div style={{ height: 500, width: '100%', padding: '0 50px', marginTop: '50px' }}>
       <DataGrid
-        rows={rows}
+        rows={shownCourses}
         columns={columns}
         disableRowSelectionOnClick={true}
         initialState={{
--- taxi-frontend/src/store/rootReducer.ts
+++ taxi-frontend/src/store/rootReducer.ts
@@ -2,5 +2,6 @@ import { combineReducers } from "@reduxjs/toolkit"
 import authReducer from "./auth/authSlice.ts"
 import mapReducer from "./map/mapSlice"
 import taxiReducer from "./taxis/taxiSlice"
+import courseReducer from "./course/courseSlice"
 
-export const createReducer = () => combineReducers({ authReducer, mapReducer, taxiReducer })
+export const createReducer = () => combineReducers({ authReducer, mapReducer, taxiReducer, courseReducer })
