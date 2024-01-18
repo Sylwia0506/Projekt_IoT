7271f7c9aadeb0028a74ad9e98c4dcfbc63afe14
Tennessee-Git <awramiukofficial@gmail.com>
2024-01-18 13:36:41 +0100

    Changed taxi id to registration in Courses

--- taxi-frontend/src/pages/courses/Courses.tsx
+++ taxi-frontend/src/pages/courses/Courses.tsx
@@ -1,34 +1,53 @@
-import { DataGrid } from '@mui/x-data-grid';
-import { columns } from './mockCourses';
-import { useAppDispatch, useAppSelector } from '../../store/hooks';
-import { courseLoading, courseSelector, getCourses } from '../../store/course/courseSlice';
-import { useEffect, useState } from 'react';
-import { CourseBody } from '../../store/course/types/courseTypes';
-
+import { DataGrid } from "@mui/x-data-grid"
+import { columns } from "./mockCourses"
+import { useAppDispatch, useAppSelector } from "../../store/hooks"
+import {
+  courseLoading,
+  courseSelector,
+  getCourses,
+} from "../../store/course/courseSlice"
+import { useEffect, useState } from "react"
+import { CourseBody } from "../../store/course/types/courseTypes"
+import { getTaxis, taxisSelector } from "../../store/taxis/taxiSlice"
 
 function Courses() {
   const dispatch = useAppDispatch()
+  const taxis = useAppSelector(taxisSelector)
   const courses = useAppSelector(courseSelector)
   const loading = useAppSelector(courseLoading)
   const [shownCourses, setShownCourses] = useState<CourseBody[]>([])
 
-  
   useEffect(() => {
     void dispatch(getCourses())
+    void dispatch(getTaxis())
   }, [dispatch])
 
   useEffect(() => {
-    if (courses) {
-      setShownCourses(courses.slice())
+    if (courses && taxis) {
+      setShownCourses(
+        courses.map((c) => {
+          return {
+            ...c,
+            taxi: taxis.find((t) => t.id == c.taxi)?.registration as string,
+          }
+        })
+      )
     }
-  }, [courses])
+  }, [courses, taxis])
 
   if (loading) {
     return <div>Loading...</div>
   }
-  
+
   return (
-    <div style={{ height: 500, width: '100%', padding: '0 50px', marginTop: '50px' }}>
+    <div
+      style={{
+        height: 500,
+        width: "100%",
+        padding: "0 50px",
+        marginTop: "50px",
+      }}
+    >
       <DataGrid
         rows={shownCourses}
         columns={columns}
@@ -40,7 +59,7 @@ function Courses() {
         }}
       />
     </div>
-  );
+  )
 }
 
 export default Courses
